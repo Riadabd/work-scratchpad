ae8c4d0ce776663e4b7e827f6f744647d90c038f
Riad Abdallah <riad.abdallah@hotmail.com>
2025-01-08 20:26:50 +0100

    Add config file for constructing the delete queries

---
+++ delete-organization/python/config/loket/path_to_types.py
@@ -0,0 +1,124 @@
+data = {
+    "delete": [
+        {
+            "type": "http://data.vlaanderen.be/ns/besluit#Bestuursorgaan",
+            "pathToType": """
+                ?resource <http://data.vlaanderen.be/ns/besluit#bestuurt> ?organization .
+            """,
+            "additionalFilter": """FILTER(?g != <http://mu.semte.ch/graphs/landing-zone/op-public>)""",
+        },
+        {
+            "type": "http://data.vlaanderen.be/ns/besluit#Bestuursorgaan",
+            "pathToType": """
+                ?resource <https://data.vlaanderen.be/ns/generiek#isTijdspecialisatieVan> ?bestuursorgaan .
+                ?bestuursorgaan <http://data.vlaanderen.be/ns/besluit#bestuurt> ?organization .
+            """,
+            "additionalFilter": """FILTER(?g != <http://mu.semte.ch/graphs/landing-zone/op-public>)""",
+        },
+        # Covers the cases when ?p = <http://schema.org/recipient> or ?p = <http://schema.org/sender>
+        {
+            "type": "http://schema.org/Message",
+            "pathToType": """
+                ?resource ?p ?organization
+            """,
+            "additionalFilter": "",  # Messages can point to files (that relationship will be inside the public graph), so we do not specify any graph filter to delete this relationship.
+        },
+        # Files connected to messages
+        {
+            "type": "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject",
+            "pathToType": """
+                ?message a <http://schema.org/Message> ;
+                    ?p ?organization ;
+                    <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#hasPart> ?resource .
+            """,
+            "additionalFilter": "",
+        },
+        # Conversations pointing to messages
+        {
+            "type": "http://schema.org/Conversation",
+            "pathToType": """
+                ?message a <http://schema.org/Message> ;
+                    ?p ?organization .
+
+                ?resource <http://schema.org/hasPart> ?message .
+            """,
+            "additionalFilter": "",
+        },
+        # Employee datasets, period slices and observations
+        {
+            "type": "http://lblod.data.gift/vocabularies/employee/EmployeeDataset",
+            "pathToType": """
+                ?resource <http://purl.org/dc/terms/creator> ?organization .
+            """,
+            "additionalFilter": "",
+        },
+        {
+            "type": "http://lblod.data.gift/vocabularies/employee/EmployeePeriodSlice",
+            "pathToType": """
+                ?dataset a <http://lblod.data.gift/vocabularies/employee/EmployeeDataset> ;
+                    <http://purl.org/dc/terms/creator> ?organization ;
+                    <http://purl.org/linked-data/cube#slice> ?resource .
+            """,
+            "additionalFilter": "",
+        },
+        {
+            "type": "http://lblod.data.gift/vocabularies/employee/EmployeeObservation",
+            "pathToType": """
+                ?dataset a <http://lblod.data.gift/vocabularies/employee/EmployeeDataset> ;
+                    <http://purl.org/dc/terms/creator> ?organization ;
+                    <http://purl.org/linked-data/cube#slice> ?slice .
+
+                ?slice <http://purl.org/linked-data/cube#observation> ?resource .
+            """,
+            "additionalFilter": "",
+        },
+        # BBCDR reports
+        {
+            "type": "http://mu.semte.ch/vocabularies/ext/bbcdr/Report",
+            "pathToType": """
+                ?resource <http://purl.org/dc/terms/subject> ?organization .
+            """,
+            "additionalFilter": """FILTER (?p != <http://mu.semte.ch/vocabularies/ext/bbcdr/package>)""", #  Preseve the package property as it helps with finding dangling files if want to deal with them at a later stage.
+        },
+        # Files connected to BBCDR reports
+        {
+            "type": "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject",
+            "pathToType": """
+                ?report a <http://mu.semte.ch/vocabularies/ext/bbcdr/Report> ;
+                    <http://purl.org/dc/terms/subject> ?organization ;
+                    <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#hasPart> ?resource .
+            """,
+            "additionalFilter": "",
+        },
+        # Submissions
+        {
+            "type": "http://rdf.myexperiment.org/ontologies/base/Submission",
+            "pathToType": """
+                ?resource <http://purl.org/pav/createdBy> ?organization .
+            """,
+            "additionalFilter": "",
+        },
+        # Submission documents
+        {
+            "type": "http://mu.semte.ch/vocabularies/ext/SubmissionDocument",
+            "pathToType": """
+                ?submission a <http://rdf.myexperiment.org/ontologies/base/Submission> ;
+                    <http://purl.org/pav/createdBy> ?organization .
+
+                ?submission <http://purl.org/dc/terms/subject> ?resource .
+            """,
+            "additionalFilter": """FILTER (?p != <http://purl.org/dc/terms/source>)""", # Preseve the source property as it helps with finding dangling files if want to deal with them at a later stage.
+        },
+        # Files connected to submissions
+        {
+            "type": "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject",
+            "pathToType": """
+                ?submission a <http://rdf.myexperiment.org/ontologies/base/Submission> ;
+                    <http://purl.org/pav/createdBy> ?organization .
+
+                ?submission <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#hasPart> ?resource .
+            """,
+            "additionalFilter": "",
+        },
+    ]
+}
